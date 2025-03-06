244555a5854a9425b2916eee3516fff6d75793a2
Robson Paulo da Silva <robson_p_s@yahoo.com.br>
2025-03-06 16:34:46 -0300

    modified:   server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 
 const mongodb = require("./data/database");
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8000;
 
 app.use("/", require("./routes"));
 
