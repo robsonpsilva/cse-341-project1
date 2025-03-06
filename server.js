279941d412b8567c36c7db6fe45672df787779fb
Robson Paulo da Silva <robson_p_s@yahoo.com.br>
2025-03-06 12:29:04 -0300

    modified:   package-lock.json 	modified:   package.json 	new file:   routes/contact.js 	new file:   routes/data/database.js 	modified:   server.js

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
 const express = require("express");
 const app = express();
 
+const mangodb = require("./data/database");
+
 const port = process.env.PORT || 3000;
 
 app.use("/", require("./routes"));
 
-app.listen(port, () =>{console.log(`Running on port ${port}`)});
+
+mongodb.initDb((err) =>{
+    if(err){
+        console.log(err);
+    }
+    else{
+        app.listen(port, () =>{console.log(`Database and Node running on port ${port}`)});
+    }
+})
