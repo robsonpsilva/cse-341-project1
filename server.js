f5f3a989eca8ea7bd30af13cc40bdf83cc4499d9
Robson Paulo da Silva <robson_p_s@yahoo.com.br>
2025-03-06 09:07:47 -0300

    modified:   .gitignore 	new file:   routes/index.js 	modified:   server.js

--- server.js
+++ server.js
@@ -1,4 +1,8 @@
 const express = require("express");
 const app = express();
 
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
+
+app.use("/", require("./routes"));
+
+app.listen(port, () =>{console.log(`Running on port ${port}`)});
