512010acff6c4760801ae980ac636ce9ced29b16
Robson Paulo da Silva <robson_p_s@yahoo.com.br>
2025-03-08 07:53:37 -0300

    modified:   routes/index.js

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 
-router.get("/", (req,res) => {res.send("Hello  World - BYU - Paulo!");});
+router.get("/", (req,res) => {res.send("Hello  World - BYU! type https://cse-341-project1-t08e.onrender.com/contacts para obter todos os contatos, ou https://cse-341-project1-t08e.onrender.com/contacts/67ca09d206dd9a333bc7be0e para recuperar o contato com id 67ca09d206dd9a333bc7be0e .");});
 router.use('/contacts', require('./contacts'));
 
 module.exports = router;
