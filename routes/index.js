e953b5197ec40a65e76090021b3527677cb19a49
Robson Paulo da Silva <robson_p_s@yahoo.com.br>
2025-03-08 08:08:32 -0300

    modified:   routes/index.js

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,19 @@
 const router = require('express').Router();
 
-router.get("/", (req,res) => {res.send("Hello  World - BYU! type https://cse-341-project1-t08e.onrender.com/contacts para obter todos os contatos, ou https://cse-341-project1-t08e.onrender.com/contacts/67ca09d206dd9a333bc7be0e para recuperar o contato com id 67ca09d206dd9a333bc7be0e .");});
+router.get("/", (req,res) => {res.send(` <html>
+            <head>
+                <title>Robson Paulo da Silva</title>
+            </head>
+            <body>
+                <h1>Welcome to Project: Contacts Part 1 Exercise</h1>
+                <p>Click on the links below</p>
+                <ul>
+                    <li><a href="https://cse-341-project1-t08e.onrender.com/contacts">All contacts</a>></li>
+                    <li><a href="https://cse-341-project1-t08e.onrender.com/contacts/67ca09d206dd9a333bc7be0e">Contact Id: ca09d206dd9a333bc7be0e </a></li>
+                </ul>
+            </body>
+        </html>`);});
 router.use('/contacts', require('./contacts'));
 
 module.exports = router;
+    `
