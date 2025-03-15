25bd043dd09ea2d787f845431dbfabaee244f3cd
Robson Paulo da Silva <robson_p_s@yahoo.com.br>
2025-03-15 00:36:24 -0300

    modified:   routes/contacts.js

--- routes/contacts.js
+++ routes/contacts.js
@@ -1,9 +1,16 @@
 const express = require("express");
 const router = express.Router();
+const { createContact } = require('../controllers/contacts');
 
 const contactsController =  require("../controllers/contacts");
 
 router.get("/", contactsController.getAll);
 router.get("/:id", contactsController.getSingle);
+router.put('/', createContact);
 
 module.exports = router;
+
+
+// Rota para lidar com o método PUT e criar um contato
+
+
